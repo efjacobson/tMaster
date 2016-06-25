0d67851f7b8b89474c6842335d7cdb3ababb9928
davidlopez <letsbelopez@gmail.com>
2016-06-25 09:36:03 -0700

    david's commit. angular initialized

---
+++ tMaster/Scripts/Sabio.js
@@ -0,0 +1,28 @@
+﻿var sabio = {
+    utilities: {}
+    , layout: {}
+    , page: {
+        handlers: {
+        }
+        , startUp: null
+    }
+    , services: {}
+    , ui: {}
+
+};
+
+
+sabio.layout.startUp = function () {
+
+    //console.debug("sabio.layout.startUp");
+
+    //this does a null check on sabio.page.startUp
+    if (sabio.page.startUp) {
+        //console.debug("sabio.page.startUp");
+        sabio.page.startUp();
+    }
+};
+
+
+
+$(document).ready(sabio.layout.startUp);
