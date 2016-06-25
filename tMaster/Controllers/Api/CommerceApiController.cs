0fc9d710d49ce3d87160fe74314f13d273043ef0
davidlopez <letsbelopez@gmail.com>
2016-06-25 13:05:45 -0700

    api controller inititialized and commerce index update

---
+++ tMaster/Controllers/Api/CommerceApiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace tMaster.Controllers.Api
+{
+    public class CommerceApiController : ApiController
+    {
+        [HttpGet]
+        public string GetOffers()
+        {
+            return "hello";
+        }
+    }
+}
