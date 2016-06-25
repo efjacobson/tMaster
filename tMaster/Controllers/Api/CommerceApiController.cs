778670d78789aa74b96f83c25f8a23b2a9725bc7
davidlopez <letsbelopez@gmail.com>
2016-06-25 13:48:21 -0700

    api controller

---
+++ tMaster/Controllers/Api/CommerceApiController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace tMaster.Controllers.Api
+{
+    [RoutePrefix("api/offers")]
+    public class CommerceApiController : ApiController
+    {
+        [HttpGet, Route("{eventID}")]
+        public HttpResponseMessage GetOffers(string eventID)
+        {
+            string json = null;
+
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    json = wc.DownloadString("https://app.ticketmaster.com/commerce/v2/events/" + eventID + "/offers.json?apikey=1NxKoxkq6DbOwXVZeGfwKMnR12D3ebFC");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(ex.ToString());
+            }
+
+
+            var d = JsonConvert.DeserializeObject(json);
+            return Request.CreateResponse(d);
+
+        }
+    }
+}
